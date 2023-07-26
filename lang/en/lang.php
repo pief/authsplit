25fee81ad66db255eaeea01d8c4d7ca75dbfec18
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2023-07-26 23:58:24 +0200

    Add French language

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -9,7 +9,7 @@ $lang['nocfg']      = 'Config error: no value found for setting "%s"!';
 $lang['pluginload'] = 'Config error: could not load auth plugin "%s"!';
 
 $lang['erraddusercap'] = 'Automatic creation of DokuWiki user accounts enabled but secondary auth plugin "%s" does not support adding users!';
-$lang['erradduserinfo'] = 'Automatic creation of DokuWiki user account failed because primary auth plugin "%s}" could not provide user info!';
+$lang['erradduserinfo'] = 'Automatic creation of DokuWiki user account failed because primary auth plugin "%s" could not provide user info!';
 
 $lang['autocreated'] = 'A DokuWiki user account has been created for you automatically. Please review your user profile.';
 
